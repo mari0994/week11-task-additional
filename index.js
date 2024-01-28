2f97f396e54053d6e8e2edda92391e8b656a51b0
mari0994 <mbabiy@mail.ru>
2024-01-28 22:54:19 +0300

    correct mistake

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const sumBefore = firstPriceNumber + secondPriceNumber + thirdPriceNumber + four
 
 function calculatePrice(){
     const sumDiscount = document.querySelector('.total-price-quantity');
-    sumDiscount.innerHTML = sumBefore - sumBefore * 0.2;
+    sumDiscount.innerText = sumBefore - sumBefore * 0.2;
 }
 
 const button = document.querySelector('.btn-calculate');
