2ca068290adb4dbaff54ae2309df195d730dcdd0
mari0994 <mbabiy@mail.ru>
2024-02-02 00:25:14 +0300

    add

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const sumBefore = firstPriceNumber + secondPriceNumber + thirdPriceNumber + four
 
 function calculatePrice(){
     const sumDiscount = document.querySelector('.total-price-quantity');
-    sumDiscount.textContent = sumBefore - sumBefore * 0.2;
+    sumDiscount.textContent = (sumBefore - sumBefore * 0.2).toFixed(2);
 }
 
 const button = document.querySelector('.btn-calculate');
